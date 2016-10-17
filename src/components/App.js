3821abef2753c077b10be039a8f93bd4b199c582
Guy Thomas <guythomas721@gmail.com>
2016-10-17 11:49:31 -0700

    Base commit. React set up, bootstrap sourced. We are locked and loaded.

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+class App extends React.Component {
+	constructor() {
+		super(),
+
+		this.state = {
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				Test
+			</div>
+		);
+	}
+}
+
+window.App = App;
