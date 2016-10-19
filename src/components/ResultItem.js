697114c15add7df8a247a96ded2eff1286b1dd7b
Guy Thomas <guythomas721@gmail.com>
2016-10-18 23:35:31 -0700

    Add styling to button and bring course provider through

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -4,11 +4,11 @@ const ResultItem = (props) => {
 			<div className="card">
 				<img className="card-img-top" src={props.result.image} alt="Card image cap"/>
 				<div className="card-block">
-					<div className="card-title"><h4>{props.result.title}</h4></div>
+					<div className="card-title"><h4>{props.result.title}</h4><b>{props.result.platform}</b></div>
 					<div className="card-text">{props.result.description}</div>
 					<div className="quickInfo">
-						<div>{"Difficulty: " + props.result.difficulty}</div>
-						<div>{"Duration: " + props.result.duration}</div>
+						<div><b>Difficulty: </b>{props.result.difficulty} </div>
+						<div><b>Duration: </b>{props.result.duration} </div>
 					</div>
 					<a href={props.result.link} className="learn-more btn btn-primary active" role="button">Learn More</a>
 				</div>
