b99d9926212fa1ccdc0d10a951d7b7cad04cb920
Guy <guythomas721@gmail.com>
2017-03-06 20:50:25 +1100

    Complete styling for no search results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,18 @@
 const Results = (props) => {
-	return (
-		<div id="results">
-			{props.results.length > 0 ? props.results.map(result => <ResultItem result={result}/>) : 'No courses found for \'' + props.searchQuery + '\''}
-		</div>
-	)
+	if (props.results.length > 0) {
+		return (
+			<div id="results">
+				{props.results.map(result => <ResultItem result={result}/>)}
+			</div>
+		)
+	} else {
+		return (
+			<div className='no-results'>
+				No courses found for '{props.searchQuery}'
+			</div>
+			)
+	}
+	
 }
 
 window.Results = Results;
