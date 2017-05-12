39d7656146fb54b4e23d77614a3afaf844e3b05b
Guy <guythomas721@gmail.com>
2017-05-12 16:55:30 +1000

    Commit before major refactor

--- server/reqHandler.js
+++ server/reqHandler.js
@@ -9,38 +9,6 @@ exports.allCourses = function (req, res) {
 	res.send('allCourses');
 }
 
-exports.filteredCourses = function (req, res) {
-	// utilities.fetchCoursera(); //Don't delete
-	// utilities.fetchUdacity(); //Don't delete
-
-	// var searchQuery = req.body.searchQuery;
-	// var splitQuery = searchQuery.trim().split(' ');
-	
-	// //Create regex for the query
-	// var regexQuery = splitQuery.join("|");
-	// var re = new RegExp(regexQuery, 'ig');
-
-	// //Query with the regex
-	// Course.find({ $or: [
-	// 	{'description': re},
-	// 	{'title': re}
-	// 	]})
-	// .then(function(results){
-	// 	return relevance({
-	// 		query: splitQuery,
-	// 		data: results,
-	// 		rankings: {
-	// 		  title: 5,
-	// 		  description: 1
-	// 		}
-	// 	});
-	// })
-	// .then(function(results){
-	// 	res.send(JSON.stringify(results.slice(0,20)));
-	// })
-}
-
-
 exports.elasticSearch = function (clientReq, clientRes) {
 	// utilities.fetchCoursera(); //Don't delete
 	// utilities.fetchUdacity(); //Don't delete
