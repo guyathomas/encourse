ddefbc309aa61b503e96bc73c1bb8cbfb1c205e1
Guy <guythomas721@gmail.com>
2017-05-15 13:23:48 +1000

    Add circleci integration

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+SHA1=$1
+ACCESS_ID=$2
+SECRET=$3
+
+# Create new Elastic Beanstalk version
+EB_BUCKET=encourse-bucket
+DOCKERRUN_FILE=Dockerrun.aws.json
+# EBEXTENSIONS=.ebextensions
+
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws configure set default.region us-west-1
+aws configure set aws_access_key_id $ACCESS_ID
+aws configure set aws_secret_access_key $SECRET
+# aws configure set aws_secret_access_key default_secret_key
+
+# zip -r $SHA1.zip $DOCKERRUN_FILE $EBEXTENSIONS
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+
+aws elasticbeanstalk create-application-version --application-name encourse-app \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name encourse-prod \
+    --version-label $SHA1
